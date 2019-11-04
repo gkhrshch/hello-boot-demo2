fdc657cc367e8df08663b52ab8e16ac5222042db
gkhrshch <george.khruschov@gmail.com>
2019-11-04 20:46:04 +0200

    Initial commit

---
+++ src/main/java/mate/academy/boot/hellobootdemo2/HelloBootDemo2Application.java
@@ -0,0 +1,13 @@
+package mate.academy.boot.hellobootdemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBootDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBootDemo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/boot/hellobootdemo2/HelloBootDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.boot.hellobootdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBootDemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
